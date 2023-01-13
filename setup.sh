f2e42f6b78ca0901ef17c6acc7abc74071631829
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2023-01-13 07:48:58 +0100

    setup and requirements

---
+++ setup.sh
@@ -0,0 +1,4 @@
+conda create --name=htw_project python=3.10 -y
+conda activate htw_project
+conda install -n htw_project ipykernel --update-deps --force-reinstall
+pip install -r requirements.txt --user
